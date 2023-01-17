301d529189fc6d2e23b78e2ae0c29da064798673
Iman Zahid <97961673+ImanZahid@users.noreply.github.com>
2023-01-17 01:25:46 +0100

    Add files via upload

---
+++ img/db.js
@@ -0,0 +1,11 @@
+const people = [
+    { id:128, name : 'Cindy' , lastname : 'Erickson' , favorites : [126,124]},
+    { id:124, name : 'Mary' , lastname : 'Norris' , favorites : [123, 128]},
+    { id:130, name : 'Isaac' , lastname : 'Newton' , favorites : [125, 127,128, 129]},
+    { id:123, name : 'Austin' , lastname : 'Shelton' , favorites : [125,127]},
+    { id:126, name : 'Wayne' , lastname : 'Norman' , favorites : [125]},
+    { id:127, name : 'Diana' , lastname : 'Lucas' , favorites : [126, 123,125]},
+    { id:125, name : 'Harry' , lastname : 'Potter' , favorites : [123,129,126,127]},
+    { id:129, name : 'Leo' , lastname : 'Miles' , favorites : [128]},
+
+]
